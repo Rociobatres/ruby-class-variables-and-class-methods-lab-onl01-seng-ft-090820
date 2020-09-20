f9159d6c422936683a9bee7a06e9cf70e876c621
Rocio Batres <jonasgirl21@gmail.com>
2020-09-20 20:22:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,10 @@ class Song
     @@genre = [] 
     
     def intialize(name, artist, genre)
-      @@
+      @name= name 
+      @artist= artist
+      @genre= genre 
+      
       
     end   
 end   
